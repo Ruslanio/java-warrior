3a3f9f50ea96af9631ae8f669d4c48b5b5765f2f
rmirkhusianov <mirhusainow@gmail.com>
2018-04-04 09:58:05 +0300

    Initial commit

---
+++ src/router/index.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Authentication from '@/components/pages/Authentication/Authentication.vue'
+Vue.use(Router)
+export default new Router({
+  routes: [
+    {
+      path: '/login',
+      name: 'Authentication',
+      component: Authentication
+    }
+  ]
+})
